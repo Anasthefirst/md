b5d540111bfa4945f34a4ee93a757cc3e898a64c
Anasthefirst <Anasrefa2@gmail.com>
2025-01-17 12:14:59 -0600

    Update fly.js

--- fly.js
+++ fly.js
@@ -1,34 +1,51 @@
-var newspeed = 10
+(function() {
+  // Disable fall damage
+  ModAPI.require("player");
+  ModAPI.require("network");
 
-var oldspeed = ModAPI.player.getSpeed()
-ModAPI.addEventListener("update", function (){
-//    console.log(ModAPI.player.isMoving())
-    if(ModAPI.player.isMoving() == true){
-        ModAPI.player.setSpeed({speed: newspeed})
-    } else {
-        ModAPI.player.setSpeed({speed: oldspeed})
+  ModAPI.addEventListener("update", () => {
+    if (ModAPI.player.fallDistance > 2.0) {
+      ModAPI.network.sendPacketPlayer({ isOnGround: true });
     }
-})
+  });
 
+  // Variables for speed and jump height
+  let normalSpeed = 5;
+  let fastSpeed = 10;
+  let slowSpeed = 2;
+  let jumpHeight = 1;
+  let maxJumpHeight = 5;
+  let minJumpHeight = 1;
 
-ModAPI.require("player"); //Require the player, we need to see their fall height.
-ModAPI.require("network"); //Require the network, we need to send network packets.
-
-ModAPI.addEventListener("update", ()=>{ // Every client tick
-  if (ModAPI.player.fallDistance > 2.0) { // If the player is at a height that they can take damage from hitting the ground:
-    ModAPI.network.sendPacketPlayer({isOnGround: true}); // Tell the server the player is on the ground
-  }
-});
-
-let height = 0;
-let maxJumpHeight = 3;
-document.addEventListener('keydown', function(event) {
-  if (event.code == 'Space' && ModAPI.mcinstance.$currentScreen === null) {
-    if (height < maxJumpHeight) {
-        ModAPI.mcinstance.$thePlayer.$motionY = 1;
-        height += 1;
-    } else if (ModAPI.mcinstance.$thePlayer.$isCollidedVertically === 1){
-        height = 0;
+  // Adjust speed with M and N keys
+  document.addEventListener('keydown', function(event) {
+    if (event.code == 'KeyM') {
+      ModAPI.player.setSpeed({ speed: fastSpeed });
+    } else if (event.code == 'KeyN') {
+      ModAPI.player.setSpeed({ speed: slowSpeed });
     }
-  }
-});
+  });
+
+  // Adjust jump height with B and V keys
+  document.addEventListener('keydown', function(event) {
+    if (event.code == 'KeyB') {
+      if (jumpHeight < maxJumpHeight) {
+        jumpHeight += 1;
+      }
+    } else if (event.code == 'KeyV') {
+      if (jumpHeight > minJumpHeight) {
+        jumpHeight -= 1;
+      }
+    }
+  });
+
+  // Implement the jump height adjustment
+  document.addEventListener('keydown', function(event) {
+    if (event.code == 'Space' && ModAPI.mcinstance.$currentScreen === null) {
+      if (ModAPI.mcinstance.$thePlayer.$motionY === 0) {
+        ModAPI.mcinstance.$thePlayer.$motionY = jumpHeight;
+      }
+    }
+  });
+
+})();
