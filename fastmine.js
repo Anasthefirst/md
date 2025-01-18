fbaa17ebcc12badd12b2b5ba4a7c060b6adcac18
Anasthefirst <Anasrefa2@gmail.com>
2025-01-18 13:16:25 -0600

    Update fastmine.js

--- fastmine.js
+++ fastmine.js
@@ -1,1 +1,12 @@
+(function() {
+  ModAPI.require("player");
+  ModAPI.require("network");
 
+  // Enable instant mining
+  ModAPI.addEventListener("update", () => {
+    if (ModAPI.mcinstance.$thePlayer) {
+      ModAPI.mcinstance.$thePlayer.capabilities.isCreativeMode = true;
+      ModAPI.mcinstance.$thePlayer.capabilities.allowEdit = true;
+    }
+  });
+})();
