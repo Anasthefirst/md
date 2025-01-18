f922f1f925146a4ee6cf44040752d8826c36f45a
Anasthefirst <Anasrefa2@gmail.com>
2025-01-18 08:21:00 -0600

    Update fly.js

--- airjump.js
+++ airjump.js
@@ -3,12 +3,6 @@
   ModAPI.require("player");
   ModAPI.require("network");
 
-  ModAPI.addEventListener("update", () => {
-    if (ModAPI.player.fallDistance > 2.0) {
-      ModAPI.network.sendPacketPlayer({ isOnGround: true });
-    }
-  });
-
   // Toggle jumping in the air with G key
   let canJumpInAir = false;
   document.addEventListener('keydown', function(event) {
@@ -24,6 +18,33 @@
       if (canJumpInAir || ModAPI.mcinstance.$thePlayer.$motionY === 0) {
         ModAPI.mcinstance.$thePlayer.$motionY = 0.42; // Normal jump height in Minecraft
       }
+    }
+  });
+
+  // Implement flying mechanics
+  let canFly = false;
+  document.addEventListener('keydown', function(event) {
+    if (event.code == 'KeyF') {
+      canFly = !canFly;
+      console.log("Can fly: " + canFly);
+    }
+  });
+
+  document.addEventListener('keydown', function(event) {
+    if (canFly) {
+      if (event.code == 'Space') { // Fly up
+        ModAPI.mcinstance.$thePlayer.$motionY = 0.42;
+      }
+      if (event.code == 'ShiftLeft' || event.code == 'ShiftRight') { // Fly down
+        ModAPI.mcinstance.$thePlayer.$motionY = -0.42;
+      }
+    }
+  });
+
+  // Stop vertical motion when keys are released
+  document.addEventListener('keyup', function(event) {
+    if (canFly && (event.code == 'Space' || event.code == 'ShiftLeft' || event.code == 'ShiftRight')) {
+      ModAPI.mcinstance.$thePlayer.$motionY = 0;
     }
   });
 
